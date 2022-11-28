93ee333dc94d8851f91ccb2b29fd02e9098dcd4d
lberesford <65384348+lberesford@users.noreply.github.com>
2022-11-28 12:20:18 -0700

    Add files via upload

--- NFLSalaryGUI.py
+++ NFLSalaryGUI.py
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
+import psycopg2
+
 window = tk.Tk()
 window.title('Welcome to NFL Salary Database!')
 window.geometry('1280x720')
@@ -35,38 +37,28 @@ tabControl.add(positionTab, text = 'Positions')
 tabControl.add(coachTab, text = 'Coaches')
 tabControl.add(mascotTab, text = 'Mascots')
 tabControl.pack(expand = 1, fill = 'both')
-#header.pack()
-#header.grid(column = 11, row = 0)
 
-#def playersClick():
-#header = tk.Label(mainTab, text = 'NFL Salaries', fg = 'white', bg = 'black')
-#header.pack()
 ttk.Label(mainTab, text = 'CSE412 Group 8', font = ('Arial', 25)).pack()
 ttk.Label(mainTab, text = '').pack()
 ttk.Label(mainTab, text = 'Darin Thornton').pack()
 ttk.Label(mainTab, text = 'Jacob Nootens').pack()
 ttk.Label(mainTab, text = 'Lauren Beresford').pack()
 
-#playerBtn = tk.Button(mainTab, text = 'Players')
-#playerBtn.grid(column = 0, row = 12)
-#coachBtn = tk.Button(mainTab, text = 'Coaches')
-#coachBtn.grid(column = 8, row = 12)
-#mascotBtn = tk.Button(mainTab, text = 'Mascots')
-#mascotBtn.grid(column = 16, row = 12)
-#playerBtn.pack()
-#coachBtn.pack()
-#mascotBtn.pack()
 
 ###team tab
-teams = ['Ravens','Bills','Panthers','Bears','Bengals','Browns',
+teams = ['Cardinals','Ravens','Bills','Panthers','Bears','Bengals','Browns',
          'Cowboys','Broncos','Lions','Packers','Texans','Colts',
          'Jaguars','Chiefs','Raiders','Chargers','Rams','Dolphins',
          'Vikings','Patriots','Saints','Giants','Jets','Eagles',
          'Steelers','49ers','Seahawks','Buccaneers','Titans','Commanders']
-avgSalaries = [420,69,420,69,420,69,420,69,420,69,420,69,420,69,420,69,
+avgSalaries = [69,420,69,420,69,420,69,420,69,420,69,420,69,420,69,420,69,
                420,69,420,69,420,69,420,69,420,69,420,69,420,69]
 colors = ['red', 'yellow', 'black', 'blue', 'orange', 'cyan', 'green']
 
+ttk.Label(teamTab, text = 'Teams Page', font = ('Arial', 25)).pack()
+ttk.Label(teamTab, text = '').pack()
+
+#team average graph
 figure = plt.Figure(figsize=(6,5), dpi=100)
 ax = figure.add_subplot(111)
 chart_type = FigureCanvasTkAgg(figure, teamTab)
@@ -75,12 +67,56 @@ x_pos = np.arange(len(teams))
 ax.bar(x_pos, avgSalaries, color = colors)
 ax.set_title('Average Salaries by Team')
 
+#team dropdown menu
 ttk.Label(teamTab, text = '').pack()
 ttk.Label(teamTab, text = 'Select a Team').pack()
 selected = StringVar()
-selected.set('Chiefs')
-drop = OptionMenu(teamTab, selected, *teams)
+selected.set('Cardinals')
+drop = ttk.Combobox(teamTab, textvariable = selected)
+drop['values'] = teams
+drop['state'] = 'readonly'
 drop.pack()
+currentTeam = ''
+
+#print table of players
+def team_changed(event):
+    ttk.Label(teamTab, text = 'Current Team is ' + str(drop.get())).pack()
+    currentTeam = str(drop.get())
+    #print(db.execute("SELECT Player.firstName, Mascot.lastName, Player.salary 
+    #FROM Player, Team 
+    #WHERE Team.teamid = Player.teamid AND Team.name = currentTeam
+    #ORDER BY Player.salary DESC"))
+drop.bind('<<ComboboxSelected>>', team_changed)
+
+
+###positions tab
+ttk.Label(positionTab, text = 'Positions Page', font = ('Arial', 25)).pack()
+ttk.Label(positionTab, text = '').pack()
+
+positions = ['Quarterback','Running Back', 'Full Back','Wide Receiver',
+           'Tight End','Left Tackle','Left Guard','Center','Right Guard',
+           'Right Tackle','Cornerback','Strong Safety','Free Safety',
+           'Defensive End','Defensive Tackle','Outside Linebacker',
+           'Inside Linebacker','Linebacker','Kicker','Punter']
+avgPosSalaries = [69,420,69,420,69,420,69,420,69,420,69,420,69,420,69,420,69,
+               420,69,420]
+
+#team graph
+figure = plt.Figure(figsize=(6,5), dpi=100)
+ax = figure.add_subplot(111)
+chart_type = FigureCanvasTkAgg(figure, positionTab)
+chart_type.get_tk_widget().pack()
+x_pos = np.arange(len(positions))
+ax.bar(x_pos, avgPosSalaries, color = colors)
+ax.set_title('Average Salaries by Position')
+
+#team dropdown menu
+ttk.Label(positionTab, text = '').pack()
+ttk.Label(positionTab, text = 'Select a Position').pack()
+selected = StringVar()
+selected.set('Quarterback')
+drop2 = OptionMenu(positionTab, selected, *positions)
+drop2.pack()
 
 ###graph fun
 mascotNames = ['Bacon','Avocado','Lettuce','Egg']
